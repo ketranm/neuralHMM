d52bed56b335d88aeba37c2004c9cf212214bfbf
Ke Tran <ketranmanh@gmail.com>
2016-10-28 10:55:15 +0200

    no need init

--- init.lua
+++
@@ -1,9 +0,0 @@
-require 'torch'
-require 'nn'
--- global table deep structure neural network
-dsnn = {}
-dsnn.version = 1
-
-unpack = unpack or table.unpack -- lua 5.2 compat
-
-require('dsnn.Model')
